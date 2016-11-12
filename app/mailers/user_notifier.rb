f4b51e1f3103640d284e54f6a5cd56c250074f68
Tim Hall <timothypatrickhall@gmail.com>
2016-11-12 23:36:34 +0000

    Fixed login link in email v2 and filtered email recipient

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -4,7 +4,7 @@ class UserNotifier < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_update_email(lesson)
     @lesson = lesson
-     @lesson.course.users.each do |lcu|
+     @lesson.course.users.first do |lcu|
     mail( :to => lcu.email,
     :subject => 'There is an update in your user area.' )
     end
